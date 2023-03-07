96df1ca6ec35d0ac42d1a5b872e0f2902176ad20
Yannis Storrer <yannis.storrer@web.de>
2023-03-07 19:14:42 +0100

    TUI interface
    implemented the logic of a basic tui interface with input handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,74 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, thread, time::Duration, sync::mpsc, time::Instant};
+use tui::{backend::CrosstermBackend,
+    Terminal,
+    widgets::{Widget, Block, Borders}};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+enum Event<I> {
+    Input(I),
+    Tick,
 }
+
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+
+    // setup input handler
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let crossterm::event::Event::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    loop{
+        terminal.draw(|f| {
+            let size = f.size();
+            let block = Block::default()
+                .title("mplayer")
+                .borders(Borders::ALL);
+            f.render_widget(block, size);
+        })?;
+        match rx.recv().unwrap() {
+            Event::Input(event) => match event.code {
+                KeyCode::Char('q') => {break},
+                _ => {}
+            }
+            Event::Tick => {}
+        }
+    }
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+    Ok(())
+}
+
