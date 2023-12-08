2d97c64b9d06f576eb74db1972ad2f0c0e8aa398
Yannis Storrer <yannis.storrer@web.de>
2023-12-08 11:08:19 +0100

    Sink testing
    Implemented logic to test if it possible to create a sink. If the
    program isnt able to create a sink it will panic.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ mod ui;
 use std::io;
 use ui::interface;
 use ui::{input::create_input_channel, interface::setup_terminal};
+use rodio::{Sink, OutputStream};
 
 
 
 #[tokio::main]
-async fn main() -> Result<(), io::Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // test if the audio sink works
+    test_sink()?;
+
     // setup input handler
     let rx = create_input_channel();
 
@@ -21,5 +25,16 @@ async fn main() -> Result<(), io::Error> {
     // restore terminal
     interface::restore_terminal(&mut terminal)?;
 
+    Ok(())
+}
+
+/**
+ * Code for testing if the audio sink can be owned by the program, because checking in the thread
+ * owning the sink is pain.
+ */
+fn test_sink() -> Result<(), Box<dyn std::error::Error>> {
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+    let sink = Sink::try_new(&stream_handle)?;
+    drop(sink);
     Ok(())
 }
